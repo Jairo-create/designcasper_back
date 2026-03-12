2b93515c7e015e55818ae15e5be6397a80e37b85
Jairo Sandoval Andrade <jairo880709@gmail.com>
2026-03-12 17:52:40 -0500

    actualización mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,11 +2,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
-  }
+  },
+
+  family: 4 // 🔴 fuerza IPv4 (soluciona ENETUNREACH en Render)
 });
 
 module.exports = transporter;
